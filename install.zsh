cfb7f69ac152db39921f5a17f05b6fcb264c7cac
Fernando Takai <fernando.takai@gmail.com>
2010-01-29 16:29:36 -0200

    Changed the place where we replace .zshrc

--- install.zsh
+++ install.zsh
@@ -1,15 +1,15 @@
-if [[ -f ~/.zshrc ]]; then
-    rm -fr ~/.zshrc
-fi
-
-ln -s "$(pwd)/zshrc" ~/.zshrc
-
 if [[ ! -d ~/.oh-my-zsh ]]; then
     echo "You don't have oh my zsh, installing..."
     wget http://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
     cp ./hg.zsh ~/.oh-my-zsh/lib/
 fi
 
+if [[ -f ~/.zshrc ]]; then
+    rm -fr ~/.zshrc
+fi
+
+ln -s "$(pwd)/zshrc" ~/.zshrc
+  
 # Remove any version of the old theme
 rm -fr ~/.oh-my-zsh/themes/takai.zsh-theme
 
